dd76b33d3c57e3753aadec642aa9c6d8b8cbe007
Vivek_052 <85784853+7310038081@users.noreply.github.com>
2024-11-26 22:42:31 +0530

    Update views.py

--- Project/textutils/textutils/views.py
+++ Project/textutils/textutils/views.py
@@ -6,12 +6,16 @@ def index(request):
     return render(request,'index.html')
 
 def analyze(request):
-    dj_text =request.GET.get('text','default')
-    print(dj_text)
+    dj_text =request.GET.get('text','default') #Get the text 
 
+    #check the checkbox values 
     removepunc =request.GET.get('removepunc','off')
-    print(removepunc)
-   
+    fullcaps =request.GET.get('fullcaps','off')
+    newlineremover = request.GET.get('newlineremover','off')
+    extraspaceremover = request.GET.get('extraspaceremover','off')
+    charcount = request.GET.get('charcount','off')
+
+   #check which checkbox is on 
     if removepunc == "on":
  
         punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
@@ -22,11 +26,53 @@ def analyze(request):
         
         vivek = {'purpose':'Remove Punctuations','analyzed_text':analyzed}
         return render(request,'analyze.html',vivek)
+    
+    elif(fullcaps=="on"):
+        analyzed=""
+        for char in dj_text:
+            analyzed = analyzed+ char.upper()
+        
+        vivek = {'purpose':'Change to Uppercase','analyzed_text':analyzed}
+        return render(request,'analyze.html',vivek)
+    
+    elif(newlineremover == "on"):
+        analyzed=""
+        for char in dj_text:
+            if char != "\n":
+                analyzed = analyzed + char
+
+        vivek = {'purpose':'New line remover','analyzed_text':analyzed}
+        return render(request,'analyze.html',vivek)
+    
+    elif(extraspaceremover == "on"):
+        analyzed = ""
+        for index, char in enumerate(dj_text):
+            #The enumerate function is used to get both the character (char) and its position (index) in the string.
+            if index < len(dj_text) - 1 and dj_text[index] == " " and dj_text[index + 1] == " ":
+                pass
+            #This part checks if the current character (dj_text[index]) is a space and if the next character (dj_text[index + 1]) is also a space.
+            # index < len(dj_text) - 1 ensures that we are not trying to access a character that doesn't exist
+            # (i.e., it avoids going beyond the end of the string).
+            else:
+                analyzed = analyzed+ char
+
+        vivek = {'purpose':'Extra space  remover','analyzed_text':analyzed}
+        return render(request,'analyze.html',vivek)
+
+    elif(charcount == "on"):
+        count = 0
+        for char in dj_text:
+            
+                if char!= " ":
+                    count = count +1
+
+        vivek = {'purpose':'Counting Characters','analyzed_text':count}
+        return render(request,'analyze.html',vivek)
 
     else:
         return HttpResponse("Eroor")
 
-    
+
 
 ##################### This is the navigator code ##############################
 
